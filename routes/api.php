7cda43a0cc4a2275423af1b5215dad3ed1bc547a
eden-kl <wl02635565@gmail.com>
2025-01-16 14:27:16 +0800

    [add] 新增formatter route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\v1\FormatterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function () {
+    Route::post('formatter', [FormatterController::class, 'formatJson'])->middleware('ipCheck')->name('api.v1.formatter');
+});
