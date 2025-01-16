742c58dadc5ced8b83c2d082e8c55cdd8af17f31
eden-kl <wl02635565@gmail.com>
2025-01-16 17:00:12 +0800

    [add] 新增遞迴整理json response

--- app/Services/JsonFormatService.php
+++ app/Services/JsonFormatService.php
@@ -6,16 +6,45 @@ use App\Enums\StatusCode;
 
 class JsonFormatService
 {
+    const SPACE_GAP = 4;
     /**
      * @param string $jsonString
      * @return array
      */
     public function jsonFormat(string $jsonString): array
     {
-        $jsonString = trim(preg_replace('/\s+/', '', $jsonString));
+        $dataArray = json_decode($jsonString, true);
+        $response = $this->createBeautifulJson($dataArray);
         return [
             'status' => StatusCode::allSuccess->value,
-            'data' => $jsonString,
+            'data' => $response,
         ];
+    }
+
+    /**
+     * @param array|string $data
+     * @param int $rank
+     * @return string
+     */
+    private function createBeautifulJson(array|string $data, int $rank = 0): string
+    {
+        if (gettype($data) !== 'array') {
+            $prefix = str_repeat(' ', $rank * self::SPACE_GAP);
+            return $prefix . $data;
+        }else{
+            $section = '';
+            $prefix = str_repeat(' ', $rank * self::SPACE_GAP);
+            $section .= $prefix . '{\n';
+            $lastArrayKey = array_key_last($data);
+            foreach ($data as $key => $value) {
+                $section .= $this->createBeautifulJson($value, $rank + 1);
+                if ($key !== $lastArrayKey) {
+                    $section .= ',';
+                }
+                $section .= '\n';
+            }
+            $section .= '}';
+            return $section;
+        }
     }
 }
