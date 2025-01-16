2c4534ce90f23c719e1cd8d935f8cfab09e36b6f
eden-kl <wl02635565@gmail.com>
2025-01-16 15:03:28 +0800

    [add] 新增validation

--- app/Http/Controllers/Api/v1/FormatterController.php
+++ app/Http/Controllers/Api/v1/FormatterController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Exceptions\HttpRequestCustomException;
 use App\Http\Controllers\Controller;
+use App\Validations\HttpRequestValidation;
 use Illuminate\Http\Request;
 
 class FormatterController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return void
+     * @throws HttpRequestCustomException
+     */
     public function formatJson(Request $request)
     {
+        HttpRequestValidation::checkRequest($request, config('validation_rules.formatJson'));
     }
 }
